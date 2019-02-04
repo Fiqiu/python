9593a4d0ecb6d72a018cce86e7dadedd950070a1
Junpeng <1025946044@qq.com>
2019-02-04 17:12:52 +0800

    Create readme.m

---
+++ Artificial_Intelligence_hw/readme.m
@@ -0,0 +1,2 @@
+The file contains mahjong is concerning mahjong project. They separately come from https://github.com/erreurt/MahjongKit and https://github.com/MahjongRepository/mahjong-ai.
+We make improvements in the two file by using naive bayes to reinforce the judgements of opponents waiting status and scrawler functions.
